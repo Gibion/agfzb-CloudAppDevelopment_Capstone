7d2e83b56f3bd2a234f9e13f38f25bcf564f94c2
Gibion <gibion.makiwa@gmail.com>
2024-01-04 22:05:44 -0500

    Loaded data into the database

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -50,6 +50,63 @@ class CarModel(models.Model):
         return "Name: " + self.name  
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+        # Dealer state
+        self.state = state
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership  = dealership #Id of dealership
+        self.name = name #name of reviewer
+        self.purchase = purchase #if they purchased
+        self.review = review #text of review
+        self.purchase_date = purchase_date #the purchase date
+        self.car_make = car_make #car make
+        self.car_model = car_model #car model
+        self.car_year = car_year #car year
+        self.sentiment = sentiment #sentiment of the review
+        self.id = id #id of the review
+
+    def __str__(self):
+        return "Reviewer Name: " + self.name + \
+            "Review: " + self.review
+
+class ReviewPost:
+
+    def __init__(self, dealership, name, purchase, review):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
